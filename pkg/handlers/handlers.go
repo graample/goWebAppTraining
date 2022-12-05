f2a5b4a8e714c19d83470af50d840395c53e1b70
Raymond Yee <haesik.yee@gmail.com>
2022-12-05 13:58:34 -0800

    completes section 5

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,10 +1,11 @@
 package handlers
 
 import (
+	"net/http"
+
 	"github.com/graample/goWebAppTraining/pkg/config"
 	"github.com/graample/goWebAppTraining/pkg/models"
 	"github.com/graample/goWebAppTraining/pkg/render"
-	"net/http"
 )
 
 // Repo the repository used by the handlers
@@ -29,6 +30,9 @@ func NewHandlers(r *Repository) {
 
 // Home is the home page handler
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
 	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
@@ -37,6 +41,10 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	// perform some logic
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hello, again."
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+
+	stringMap["remote_ip"] = remoteIP
 
 	// send the data to the template
 	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
